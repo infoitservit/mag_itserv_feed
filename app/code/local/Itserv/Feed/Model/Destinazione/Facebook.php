7555df9a98a979596bb3d9bc8797451393ae69a8
Denis <info@it-serv.it>
2016-09-29 16:55:23 +0200

    fix facebook feed

--- app/code/local/Itserv/Feed/Model/Destinazione/Facebook.php
+++ app/code/local/Itserv/Feed/Model/Destinazione/Facebook.php
@@ -7,12 +7,55 @@
  */
 
 class Itserv_Feed_Model_Destinazione_Facebook extends Itserv_Feed_Model_Destinazione_Abstract {                                                       
-    protected $_nomeNodoCatalogo = 'products';
-    protected $_nomeNodoProdotto = 'product';   
+    protected $_nomeNodoProdotto = 'item';
+    protected $_nomeNodoCatalogo = 'channel';   
     
     protected function getProdotti() {
         $_productCollection = parent::getProdotti();
         Mage::getSingleton('cataloginventory/stock')->addInStockFilterToCollection($_productCollection);
         return $_productCollection;
     }
+    
+    /**
+     * Prepara il file XML completo
+     * @return type
+     */
+    public function preparaDocXml() {        
+        $doc = $this->creaDocXml();
+        
+        $rss = $doc->createElement('rss');
+        $rss = $doc->appendChild($rss);
+
+        $rss_version = $doc->createAttribute('version');
+        $rss->appendChild($rss_version);
+
+        $rss_version_value = $doc->createTextNode('2.0');
+        $rss_version->appendChild($rss_version_value);
+
+        $rss_google = $doc->createAttribute('xmlns:g');
+        $rss->appendChild($rss_google);
+
+        $rss_google_value = $doc->createTextNode('http://base.google.com/ns/1.0');
+        $rss_google->appendChild($rss_google_value);
+
+        $channel = $doc->createElement($this->_nomeNodoCatalogo);
+        $rss->appendChild($channel);
+
+        $title = $doc->createElement('title');
+        $title = $channel->appendChild($title);
+        $title_value = $doc->createTextNode('Feed NoiHB.it');
+        $title_value = $title->appendChild($title_value);
+
+        $link = $doc->createElement('link');
+        $link = $channel->appendChild($link);
+        $link_value = $doc->createTextNode($this->_urlPath);
+        $link_value = $link->appendChild($link_value);
+
+        $description = $doc->createElement('description');
+        $description = $channel->appendChild($description);
+        $description_value = $doc->createTextNode('Feed del sito web Noihb.it');
+        $description_value = $description->appendChild($description_value);
+        
+        return $doc;                
+    }       
 }
